a849967e0959e857f03a13efe02dc222a4b3ab6d
Jason Moore <87956172+jasonsmoore@users.noreply.github.com>
2021-12-01 18:17:04 -0800

    Small update to GET code

--- src/GetMovies.tsx
+++ src/GetMovies.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
+import { useState } from "react";
 import { Movie } from "./MovieInterface";
 
+const movieApiKey : string = process.env.REACT_APP_MOVIE_API_CREDS || '';
 
-export function GetMovies() : Promise<Movie[]>{
+export function GetMovies(year?: number) : Promise<Movie[]>{
+
     return axios
-        .get('https://api.themoviedb.org/3/discover/movie?api_key=397a07480729e1da40ac2ef7d2ffc3db&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&with_people=4724')
-        .then(response => response.data);
+        .get(`https://api.themoviedb.org/3/discover/movie?api_key=${movieApiKey}&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&with_people=4724&year=${year}`)
+        .then(response => {console.log(response.data)
+            return response.data
+    }); 
 }
 
 export default GetMovies;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,25 +1,34 @@
 import { Link } from "react-router-dom";
+import GetMovies from "./GetMovies";
+import { useState } from "react";
+import MovieResponse from "./MovieInterface";
 
 function Home() {
+    const [date, getDate] = useState('')
 
     return (
         <div>
             <Link to='/favorite-bacon'>Favorite Bacon</Link>
             <h1>Bacon Search!</h1>
-            <form className='searchTitle'>
+            <form className='searchTitle' onSubmit={(e) => {
+                e.preventDefault()
+                GetMovies()
+            }}>
                 <label>
                     <strong> Title: </strong>
                     <input />
                 </label>
-                <button type='submit'>Search</button>
                 <label>
                     <strong> Genre: </strong>
                     <input />
                 </label>
-                <button type='submit'>Search</button>
                 <label>
                     <strong> Release Date: </strong>
-                    <input />
+                    <input 
+                        value={date}
+                        onChange={e => getDate(e.target.value)}
+                        name='date'
+                    />
                 </label>
                 <button type='submit'>Search</button>
                 <button type='submit'>Bacon Me</button>
